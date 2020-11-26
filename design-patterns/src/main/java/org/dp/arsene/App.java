fe49c1377979ed731b3bc2553ed21adb44ffb851
Arsene <kevinkouomeu@icloud.com>
2020-11-26 08:59:54 +0100

    initial commit

---
+++ design-patterns/src/main/java/org/dp/arsene/App.java
@@ -0,0 +1,13 @@
+package org.dp.arsene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

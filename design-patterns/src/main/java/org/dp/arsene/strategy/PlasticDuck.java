73148a06820736040ff4f5ede853ac5e2eacbe03
Arsene <kevinkouomeu@icloud.com>
2020-11-26 09:38:33 +0100

    [Strategy Pattern] - Redefine Quack Method

--- design-patterns/src/main/java/org/dp/arsene/strategy/PlasticDuck.java
+++ design-patterns/src/main/java/org/dp/arsene/strategy/PlasticDuck.java
@@ -3,6 +3,11 @@ package org.dp.arsene.strategy;
 public class PlasticDuck extends Duck{
 
     @Override
+    public void quack() {
+       // Not Quack, but Gossip
+    }
+
+    @Override
     public void show() {
         // Show Plastic Duck
     }

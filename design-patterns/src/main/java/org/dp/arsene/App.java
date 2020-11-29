31fb24574ca7e11124ddd69cac246a70d079246e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-11-29 22:24:12 +0100

    add .iml file

--- design-patterns/src/main/java/org/dp/arsene/App.java
+++ design-patterns/src/main/java/org/dp/arsene/App.java
@@ -11,22 +11,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("//  SHOPPING STRATEGY  //\n");
-
-        final var items = new ArrayList<>(List.of(
-                new Item("DEG44515EE4", "AirPods Pro", "wireless earphones by apple", 270),
-                new Item("DEG4XQXD778", "Ipad Pro", "Tablet by apple", 670)
-        ));
-
-        final var cart = new ShoppingCart(items);
-        cart.pay(new CreditCardStrategy("Arsene", "4151 2598 4756 6633", "854", LocalDate.now()));
-
-        cart.addItem(new Item("DS9985SAZZ", "Magic KeyBoard", "keyboard apple", 130));
-        cart.pay(new PaypalStrategy("kakarot@hippy.fr", "$dif4787984/d4f"));
-
-
-        System.out.print("\n/  Generics  /");
-        List<String> l ;
+        System.out.println("// ? Pattern  //\n");
 
     }
 }

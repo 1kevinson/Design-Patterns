b6e61847c507c48fd642ac2d13c702e9b57c6b0d
Arsene Kevin <kevinkouomeu@icloud.com>
2020-12-01 00:38:29 +0100

    [Builder strategy] - Faceted builder

---
+++ design-patterns/src/main/java/org/dp/arsene/builder/User.java
@@ -0,0 +1,104 @@
+package org.dp.arsene.builder;
+
+public class User {
+    // address
+    public String streetAddress, postCode, city;
+
+    // employment
+    public String companyName, position;
+    public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postCode='" + postCode + '\'' +
+                ", city='" + city + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", annualIncome=" + annualIncome +
+                '}';
+    }
+}
+
+// Builder Facade
+class UserBuilder {
+
+    protected User user = new User();
+
+    public UserAddressBuilder lives() {
+        return new UserAddressBuilder(user);
+    }
+
+    public UserJobBuilder works() {
+        return new UserJobBuilder(user);
+    }
+
+    public User build() {
+        return user;
+    }
+}
+
+class UserAddressBuilder extends UserBuilder {
+
+    // We need a reference of this user in every sub-builder
+    public UserAddressBuilder(User user) {
+        this.user = user;
+    }
+
+    public UserAddressBuilder at(String streetAddress) {
+        user.streetAddress = streetAddress;
+        return this;
+    }
+
+    public UserAddressBuilder withPostCode(String postCode) {
+        user.postCode = postCode;
+        return this;
+    }
+
+    public UserAddressBuilder in(String city) {
+        user.city = city;
+        return this;
+    }
+}
+
+class UserJobBuilder extends UserBuilder {
+
+    public UserJobBuilder(User user) {
+        this.user = user;
+    }
+
+    public UserJobBuilder at(String companyName) {
+        user.companyName = companyName;
+        return this;
+    }
+
+    public UserJobBuilder asA(String position) {
+        user.position = position;
+        return this;
+    }
+
+    public UserJobBuilder earning(int annualIncome) {
+        user.annualIncome = annualIncome;
+        return this;
+    }
+}
+
+class Demo {
+
+    public static void main(String[] args) {
+        final var userBuilder = new UserBuilder();
+        final var user = userBuilder
+                .lives()
+                    .at("18 avenue baudelaire")
+                    .withPostCode("34422")
+                    .in("France")
+                .works()
+                    .at("Carbon IT")
+                    .asA("Software Developer")
+                    .earning(50000)
+                .build();
+
+        System.out.println(user);
+    }
+}

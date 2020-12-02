24fe28f6239fe2b3d3b57b13230b3073e0b4e2e1
Arsene Kevin <kevinkouomeu@icloud.com>
2020-12-02 05:37:44 +0100

    [Factory pattern] - Simple static factory class

--- design-patterns/src/main/java/org/dp/arsene/factory/Factory.java
+++ design-patterns/src/main/java/org/dp/arsene/factory/Factory.java
@@ -3,8 +3,8 @@ package org.dp.arsene.factory;
 public class Factory {
 
     public static void main(String[] args) {
-        final Point cartesianPoint = Point.newCartesianPoint(4, 2);
-        final Point polarPoint = Point.newPolarPoint(43, 29);
+        final Point cartesianPoint = Point.Factory.newCartesianPoint(4, 2);
+        final Point polarPoint = Point.Factory.newPolarPoint(43, 29);
 
         System.out.println(cartesianPoint);
         System.out.println(polarPoint);
@@ -21,19 +21,22 @@ class Point {
         this.y = y;
     }
 
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
-    }
-
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(rho * Math.cos(theta), theta * Math.sin(theta));
-    }
-
     @Override
     public String toString() {
         return "Point{\n" +
                 " x=" + x + ",\n" +
                 " y=" + y + "\n" +
                 "}\n";
+    }
+
+    public static class Factory {
+
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta), theta * Math.sin(theta));
+        }
     }
 }

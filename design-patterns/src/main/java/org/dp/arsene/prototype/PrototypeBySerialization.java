4b11a1bdd978a474b97780841adfa0cbd766851e
1kevinson <kevinkouomeu@icloud.com>
2020-12-06 14:18:53 +0100

    [Prototype Pattern] - Copy through Serialization

---
+++ design-patterns/src/main/java/org/dp/arsene/prototype/PrototypeBySerialization.java
@@ -0,0 +1,37 @@
+package org.dp.arsene.prototype;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.Serializable;
+
+public class PrototypeBySerialization {
+
+    public static void main(String[] args) {
+        final Foo foo = new Foo("oneT", "another");
+        final var foo2 = SerializationUtils.roundtrip(foo);
+
+        foo2.another = "jjk";
+
+        System.out.println(foo);
+        System.out.println(foo2);
+    }
+
+}
+
+
+class Foo implements Serializable {
+    public String oneThing, another;
+
+    public Foo(String oneThing, String another) {
+        this.oneThing = oneThing;
+        this.another = another;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "oneThing='" + oneThing + '\'' +
+                ", another='" + another + '\'' +
+                '}';
+    }
+}

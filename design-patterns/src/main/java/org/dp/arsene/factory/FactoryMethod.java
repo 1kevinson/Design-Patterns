23e7bd1f881a4a9ac6fbc9d0f8915abee8280a29
1kevinson <kevinkouomeu@icloud.com>
2020-12-08 02:27:09 +0100

    [Factory Method Pattern] -  FactoryMethod

---
+++ design-patterns/src/main/java/org/dp/arsene/factory/FactoryMethod.java
@@ -0,0 +1,79 @@
+package org.dp.arsene.factory;
+
+import java.util.Arrays;
+
+public class FactoryMethod {
+
+    public static void main(String[] args) {
+        final Company company = new GameDevCompany();
+        System.out.println(Arrays.toString(company.getEmployees()));
+    }
+}
+
+abstract class Company {
+
+    abstract Employee[] getEmployees();
+
+    public String createSoftware() {
+        return "Software created";
+    }
+}
+
+class GameDevCompany extends Company {
+
+    @Override
+    Employee[] getEmployees() {
+        return new Employee[]{new Designer(), new Artist()};
+    }
+}
+
+class OutSourcingCompany extends Company {
+
+    @Override
+    Employee[] getEmployees() {
+        return new Employee[]{new Programmer()};
+    }
+}
+
+interface Employee {
+    void doWork();
+}
+
+class Designer implements Employee {
+
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Designer{}";
+    }
+}
+
+class Programmer implements Employee {
+
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Programmer{}";
+    }
+}
+
+class Artist implements Employee {
+
+    @Override
+    public void doWork() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{}";
+    }
+}

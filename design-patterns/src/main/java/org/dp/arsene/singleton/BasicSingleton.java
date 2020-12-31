4915a87ac332c4edbc488417f5de24cfb813c0f6
1kevinson <kevinkouomeu@icloud.com>
2020-12-31 15:04:43 +0100

    [Singleton pattern] - add Singleton logic +steps

--- design-patterns/src/main/java/org/dp/arsene/singleton/BasicSingleton.java
+++ design-patterns/src/main/java/org/dp/arsene/singleton/BasicSingleton.java
@@ -1,16 +1,24 @@
 package org.dp.arsene.singleton;
 
-public class Singleton1 {
+public class BasicSingleton {
+
+    public static void main(String[] args) {
+
+    }
 }
 
 
 class Database {
 
+    // STEP 1 - Store the singleton instance
     private static Database instance;
 
+    // STEP 3 - Define private constructor so the class Database cannot be default initialize
     private Database() {
+        System.out.println("initializing a lazy singleton");
     }
 
+    // STEP 2 - Get the singleton instance
     public static Database getInstance() {
         if (Database.instance == null)
             Database.instance = new Database();
